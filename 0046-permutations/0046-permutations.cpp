24cef836f461f01950148d77d513199deadf02d4
Nishant Sachan <mr.nishantsachan@gmail.com>
2026-08-31 10:59:45 +0530

    Time: 3 ms (25.82%), Space: 12.4 MB (7.46%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   vector<vector<int>> ans;
+    void solve(vector<int>temp,vector<int> &nums , vector<int>&freq){
+            if(temp.size()==nums.size()){
+                ans.push_back(temp);
+                return;
+            }
+            for(int i=0;i<nums.size();i++){
+                if(freq[i]!=1){
+                    freq[i]=1;
+                    temp.push_back(nums[i]);
+                    solve(temp,nums,freq);
+                    freq[i]=0;
+                    temp.pop_back();
+                }
+                
+            }
+            
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>freq(nums.size(),0);
+        vector<int>temp;
+        solve(temp,nums,freq);
+        return ans;
+        
+    }
+};
