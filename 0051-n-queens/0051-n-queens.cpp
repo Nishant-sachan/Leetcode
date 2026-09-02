99a55b933465549c322a290c0182221f250887ce
Nishant Sachan <mr.nishantsachan@gmail.com>
2026-09-02 11:36:58 +0530

    Time: 3 ms (61.52%), Space: 10.2 MB (77.96%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,61 +1,29 @@
 class Solution {
 public:
-
-    vector<int> left;
-    vector<int> upleft;
-    vector<int> downleft;
-
-    void solve(int col, int n, vector<string>& temp,
-               vector<vector<string>>& ans) {
-
-        // All columns filled
-        if(col == n) {
+    void solve(int col, int n, vector<int> &left, vector<int> &upleft, vector<int> &downleft,
+               vector<string> &temp, vector<vector<string>> &ans) {
+        if (col == n) {
             ans.push_back(temp);
             return;
         }
-
-        for(int row = 0; row < n; row++) {
-
-            // Check if row and both diagonals are free
-            if(left[row] != 1 &&
-               upleft[row + col] != 1 &&
-               downleft[n - 1 + col - row] != 1) {
-
-                // Place Queen
+        for (int row = 0; row < n; row++) {
+            if (left[row]==0 && upleft[row+col]==0 && downleft[n-1+col-row]==0) {
                 temp[row][col] = 'Q';
+                left[row] = upleft[row+col] = downleft[n-1+col-row] = 1;
 
-                left[row] = 1;
-                upleft[row + col] = 1;
-                downleft[n - 1 + col - row] = 1;
+                solve(col+1, n, left, upleft, downleft, temp, ans);
 
-                // Move to next column
-                solve(col + 1, n, temp, ans);
-
-                // Backtrack
                 temp[row][col] = '.';
-
-                left[row] = 0;
-                upleft[row + col] = 0;
-                downleft[n - 1 + col - row] = 0;
+                left[row] = upleft[row+col] = downleft[n-1+col-row] = 0;
             }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
-
-        vector<string> temp(n, string(n, '.'));
-
         vector<vector<string>> ans;
-
-        // Row hashing
-        left.resize(n, 0);
-
-        // Both diagonals have size 2*n - 1
-        upleft.resize(2 * n - 1, 0);
-        downleft.resize(2 * n - 1, 0);
-
-        solve(0, n, temp, ans);
-
+        vector<string> temp(n, string(n, '.'));
+        vector<int> left(n,0), upleft(2*n,0), downleft(2*n,0);
+        solve(0, n, left, upleft, downleft, temp, ans);
         return ans;
     }
 };
