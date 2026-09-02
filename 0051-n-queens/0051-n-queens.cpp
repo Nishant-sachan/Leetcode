32484562b1f5b2ab5729368e88baa02b6bdbf23a
Nishant Sachan <mr.nishantsachan@gmail.com>
2026-09-02 11:28:11 +0530

    Time: 1 ms (78.13%), Space: 10.2 MB (77.96%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+bool isPossible(int row, int col ,int n, vector<string> &temp){
+    int dupRow = row;
+    int dupCol=col;
+
+    // top left 
+    while(row>=0 and col>=0){
+        if(temp[row][col]=='Q'){
+            return false;
+        }
+        row--;
+        col--;
+    }
+    // Reset row and col
+        row = dupRow;
+        col = dupCol;
+
+    // left
+        while(col>=0){
+        if(temp[row][col]=='Q'){
+            return false;
+        }
+        col--;
+    }
+    // Reset row and col
+        row = dupRow;
+        col = dupCol;
+    //bottom left
+        while(row<n and col>=0){
+        if(temp[row][col]=='Q'){
+            return false;
+        }
+        row++;
+        col--;
+    }
+    return true;
+}
+void solve(int col,int n,vector<string>&temp,vector<vector<string>>&ans){
+
+    if(col==n){
+        ans.push_back(temp);
+        return ;
+    }
+    for(int row=0;row<n;row++){
+        if(isPossible(row,col,n,temp)){
+            temp[row][col]='Q';
+            solve(col+1,n,temp,ans);
+            temp[row][col]='.';
+        }
+    }
+
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> temp(n, string(n, '.'));
+
+        vector<vector<string>> ans;
+
+        solve(0, n, temp, ans);
+
+        return ans;
+    }
+};
